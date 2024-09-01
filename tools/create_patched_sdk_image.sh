cb023143b69d1642371e9b18c75c9ebfcb89f0eb
Leptos <leptos.0.null@gmail.com>
2024-09-01 15:37:00 -0700

    Explicitly base base SDK to create_patched_sdk_image.sh

--- tools/create_patched_sdk_image.sh
+++ tools/create_patched_sdk_image.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "${1}" == "-h" ]] || [[ "${1}" == "--help" ]]; then
-    echo "Usage: ${0} <dyld_shared_cache-path> <sdk-output-path> <platform-path> <tbd-tool>"
+    echo "Usage: ${0} <dyld_shared_cache-path> <sdk-output-path> <base-sdk-path> <platform-path> <tbd-tool>"
     exit 0
 fi
 
@@ -15,17 +15,20 @@ if [[ -z "${SDK_OUTPUT_PATH}" ]]; then
     exit 1
 fi
 
-PLATFORM_PATH="${3}"
+BASE_SDK_PATH="${3}"
+if [[ -z "${BASE_SDK_PATH}" ]]; then
+    exit 1
+fi
+
+PLATFORM_PATH="${4}"
 if [[ -z "${PLATFORM_PATH}" ]]; then
     exit 1
 fi
 
-TBD_TOOL_PATH="${4}"
+TBD_TOOL_PATH="${5}"
 if [[ -z "${TBD_TOOL_PATH}" ]]; then
     exit 1
 fi
-
-LATEST_AVAILABLE_SDK="$(ls -d "${PLATFORM_PATH}/Developer/SDKs"/*.sdk | sort -V | tail -n1)"
 
 SYMBOLS_DIR="$(mktemp -d -t symbols)"
 
@@ -33,4 +36,4 @@ make dsc_extractor_client
 
 ./dsc_extractor_client "${PLATFORM_PATH}/usr/lib/dsc_extractor.bundle" "${DYLD_CACHE_PATH}" "${SYMBOLS_DIR}"
 
-./create_patched_sdk.sh "${SYMBOLS_DIR}" "${SDK_OUTPUT_PATH}" "${LATEST_AVAILABLE_SDK}" "${TBD_TOOL_PATH}"
+./create_patched_sdk.sh "${SYMBOLS_DIR}" "${SDK_OUTPUT_PATH}" "${BASE_SDK_PATH}" "${TBD_TOOL_PATH}"
